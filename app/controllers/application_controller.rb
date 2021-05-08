549544a4e40a791cf96fcb815cab1ec7a0b761ff
Kristin Ponsonby <kristinponsonby@gmail.com>
2021-05-08 08:36:05 -0400

    commit tree 8f35c1d8c9ffabeaa8ea74e3a6ea4ffe13a9dcab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,51 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
+
+  #CRUD ACTION:READ
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  #CRUD ACTION:CREATE
+  get "/articles/new" do
+    @article = Article.new
+    erb :new
+  end
+
+  
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  #CRUD ACTION:READ
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  #CRUD ACTION:UPDATE
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  #CRUD ACTION:DELETE
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+
 end
